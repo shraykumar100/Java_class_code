f494e4e140fcc78742dc402e044fd5df2f56dafa
shraykumar <shraykumar100@gmail.com>
2022-03-21 22:33:40 +0530

    21 mar class code

---
+++ 21 mar.java
@@ -0,0 +1,73 @@
+ /*
+JAVA -
+OOP - object oriented programming
+1.create a class ----- shape
+2. create a method ---- calculateAreaOfShape
+3. we have to define variables(optional) ---- side
+4. we have to create objects of the class ----- Square is an object of class Shape.
+
+
+
+objects ---- real world entities -----orange fruit
+classes ---- prototype/template -----fruit
+methods ---- behaviour ---- juicy/colorful/eatable
+variables ---- property ---- shape , color
+
+objects ---- real world entities -----square
+classes ---- prototype/template -----shape
+methods ---- behaviour ---- area/volume
+variables ---- property ---- side
+
+objects ---- real world entities -----rectangle
+classes ---- prototype/template -----shape
+methods ---- behaviour ---- area/volume
+variables ---- property ---- length, breadth
+
+
+*/
+//hello world
+//hello world
+public class HelloWorld{
+public static void main(String[] x){
+System.out.println("My name is Simmi");
+}
+
+}
+//syntax of creating a class
+//public - keyword
+//class- keyword
+//Shape - className
+/*roll number= 123 ---> int/short/long/double/float etc
+name=simmi$1---->String
+main method is always the starting point of your java application
+*/
+public class Shape{//---marking the starting of your class
+//Syntax to create a method
+//public - keyword
+//static - keyword
+//void- keyword (return type)
+//main- method name
+//(...)- arguments
+//String - datatype
+//[]- denoting array (to take more than 1 value)
+//a-name of a variable
+//;--- ending a line/statement
+public void Sample(){
+System.out.println("HELLO SAMPLE");
+System.out.println("HELLO SAMPLE");
+//Sample2();
+}
+public static void main(String[] args){//----marking start of your method
+//JAVA code
+System.out.println("HEllO world");//to print anything in console
+System.out.println("HEllO Simmi");//to print anything in console
+// Sample();//calling a method
+}//----marking end of your class
+
+
+public void Sample2(){
+System.out.println("HELLO SAMPLE2");
+
+}
+
+}//--- marking your end of the class
